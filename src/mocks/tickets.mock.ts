a8279dbd4e656cebb4472a859036369531f13736
kezu194 <robin-pimond@hotmail.fr>
2023-01-31 18:17:18 +0100

    Exercice 0 & 1 & 2

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -4,15 +4,17 @@ const dateToday: Date = new Date();
 
 export const TICKETS_MOCKED: Ticket[] = [
   {
-    title: 'SI4 in Madrid',
+    title: 'SI4 in Barcelone',
     description: '',
     date: dateToday,
-    author: 'Paul'
+    author: 'Paul',
+    major: "international"
   },
   {
     title: 'SI5 in Paris',
     description: 'Description du voyage',
     date: dateToday,
-    author: 'Anakin'
+    author: 'Anakin',
+    major: "national"
   },
 ];
